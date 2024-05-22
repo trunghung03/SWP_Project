c5c3e7fa2d6435c665ddf07e2517b5d86fa1ac0a
hoangminhle1108 <lehoangminh20041108@gmail.com>
2024-05-22 13:46:05 +0700

    upload CRUD

---
+++ Backend/DIAN_/DTOs/ProductDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace DIAN_.DTOs
+{
+    public class ProductDTO
+    {
+        public string ProCode { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public int MainDiamondId { get; set; }
+
+        public decimal ChargeUp { get; set; }
+
+        public decimal LaborPrice { get; set; }
+
+        public string ImageLinkList { get; set; }
+
+        public int SubDiamondAmount { get; set; }
+    }
+}
