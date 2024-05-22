559039d60b12a2d3ef8ee7ad4f3176f8c7518eb5
synapse26 <nguyentrunghung03@gmail.com>
2024-05-22 12:57:19 +0700

    Migrations

--- Backend/UserApplication/UserApplication/Migrations/20240522052900_Role.Designer.cs
+++ Backend/UserApplication/UserApplication/Migrations/20240522052900_Role.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserApplication.Data;
 
@@ -11,9 +12,11 @@ using UserApplication.Data;
 namespace UserApplication.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240522052900_Role")]
+    partial class Role
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -196,9 +199,6 @@ namespace UserApplication.Migrations
 
                     b.Property<bool>("PhoneNumberConfirmed")
                         .HasColumnType("bit");
-
-                    b.Property<int>("Role")
-                        .HasColumnType("int");
 
                     b.Property<string>("SecurityStamp")
                         .HasColumnType("nvarchar(max)");
--- Backend/UserApplication/UserApplication/Migrations/20240522052900_Role.cs
+++ Backend/UserApplication/UserApplication/Migrations/20240522052900_Role.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace UserApplication.Migrations
 {
     /// <inheritdoc />
-    public partial class initialCreate : Migration
+    public partial class Role : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
