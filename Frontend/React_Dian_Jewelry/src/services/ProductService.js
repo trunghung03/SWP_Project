0d4b71d2da52a321ccb7d941fca6556cc3199bf7
Duyen9904 <mimitrucduyen@gmail.com>
2024-06-15 14:28:41 +0700

    mod: refactor api

--- Frontend/React_Dian_Jewelry/src/services/ProductService.js
+++ Frontend/React_Dian_Jewelry/src/services/ProductService.js
@@ -1,29 +1,30 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://localhost:7184/api';
+// const API_BASE_URL = 'https://localhost:7184/api';
+REACT_APP_API_BASE_URL='https://diandiamond.azurewebsites.net/api';
 
 const getProductList = () => {
-    return axios.get(`${API_BASE_URL}/products/list`);
+    return axios.get(`${REACT_APP_API_BASE_URL}/products/list`);
 };
 
 const getProductDetail = (id) => {
-    return axios.get(`${API_BASE_URL}/products/${id}`);
+    return axios.get(`${REACT_APP_API_BASE_URL}/products/${id}`);
 };
 
 const getDiamondDetail = (id) => {
-    return axios.get(`${API_BASE_URL}/diamonds/${id}`);
+    return axios.get(`${REACT_APP_API_BASE_URL}/diamonds/${id}`);
 };
 
 const getCollectionDetail = (id) => {
-    return axios.get(`${API_BASE_URL}/collections/${id}`);
+    return axios.get(`${REACT_APP_API_BASE_URL}/collections/${id}`);
 };
 
 const getShellMaterials = () => {
-    return axios.get(`${API_BASE_URL}/shellmaterials`);
+    return axios.get(`${REACT_APP_API_BASE_URL}/shellmaterials`);
 };
 
 const getProductsByIds = (ids) => {
-    return axios.get(`${API_BASE_URL}/products/list`, { params: { ids: ids.join(',') } });
+    return axios.get(`${REACT_APP_API_BASE_URL}/products/list`, { params: { ids: ids.join(',') } });
 };
 
 export { getProductList, getProductDetail, getDiamondDetail, getCollectionDetail, getShellMaterials, getProductsByIds };
