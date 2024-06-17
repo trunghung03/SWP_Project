2839852dc13edc180e658b81f83971fedf76a7b9
hoangminhle1108 <147319801+hoangminhle1108@users.noreply.github.com>
2024-06-17 15:54:34 +0700

    update Querry for 5 dias each page

--- Backend/DIAN_/Helper/DiamondQuery.cs
+++ Backend/DIAN_/Helper/DiamondQuery.cs
@@ -12,6 +12,11 @@
 
         public int PageNumber { get; set; } = 1;
 
-        public int PageSize { get; set; } = 20;
+        private int _pageSize = 5;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > 0) ? value : 5;
+        }
     }
 }
