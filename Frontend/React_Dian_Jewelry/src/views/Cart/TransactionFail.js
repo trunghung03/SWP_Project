8c1f41e4074d2ecbec78a05f3ffc350ddb488ef5
NguyenHoangBaoTran <trannhbse182378@fpt.edu.vn>
2024-06-27 09:43:17 +0700

    Modify route

--- Frontend/React_Dian_Jewelry/src/views/Cart/TransactionFail.js
+++ Frontend/React_Dian_Jewelry/src/views/Cart/TransactionFail.js
@@ -9,18 +9,17 @@ import Insta from '../../components/BlogInspired/BlogInspired.js';
 import HeaderComponent from '../../components/Header/HeaderComponent';
 import FooterComponent from '../../components/Footer/FooterComponent';
 import fail from '../../assets/img/paymentFail.jpg';
+import { useCart } from '../../services/CartService';
 
-function TrasactionFail() {
+function TransactionFail() {
     const navItems = [
         { name: 'Home', link: '/home' },
         { name: 'Cart', link: '/cart' },
         { name: 'Checkout', link: '' },
-        { name: 'Trasaction Fail', link: '' }
+        { name: 'Transaction Fail', link: '' }
     ];
     const navigate = useNavigate();
-    const handleNavigate = (path, state) => {
-        navigate(path, { state });
-    };
+    const { setCartItems: updateCartContext } = useCart();
 
     useEffect(() => {
         window.scrollTo(0, 160);
@@ -29,10 +28,19 @@ function TrasactionFail() {
         const invoiceKey = `invoice${orderId}`;
         localStorage.removeItem(invoiceKey);
         localStorage.removeItem('orderId');
-    }, []);
+
+        const customerId = localStorage.getItem('customerId');
+        const cartKey = `cartItems${customerId}`;
+        localStorage.removeItem(cartKey);
+        updateCartContext([]);
+    }, [updateCartContext]);
+
+    const handleNavigate = (path, state) => {
+        navigate(path, { state });
+    };
 
     return (
-        <div className="TrasactionFail">
+        <div className="TransactionFail">
             <HeaderComponent />
             <SubNav items={navItems} />
 
@@ -41,23 +49,22 @@ function TrasactionFail() {
                     <div className='transaction_fail_content col-md-6'>
                         <h3>TRANSACTION FAILED</h3>
                         <hr className='transaction_fail_hr'></hr>
-                        <p>The payment was unssuccesfull due to some abnormality. Please try again later or use another payment method. If this is a mistake please contact us through hotline for support.</p>
+                        <p>The payment was unsuccessful due to some abnormality. Please try again later or use another payment method. If this is a mistake, please contact us through the hotline for support.</p>
                         <div className='transaction_fail_button_wrapper'>
                             <button onClick={() => handleNavigate('/diamond-jewelry', { category: 'all' })} className="transaction_fail_shop_button">Continue shopping</button>
                             <button onClick={() => handleNavigate('/contact')} className="transaction_fail_contact_button">Contact us</button>
                         </div>
                     </div>
                     <div className='transaction_fail_image col-md-6'>
-                        <img src={fail}></img>
+                        <img src={fail} alt="Transaction Failed" />
                     </div>
                 </div>
             </div>
-            <ScrollToTop></ScrollToTop>
+            <ScrollToTop />
             <Insta />
             <FooterComponent />
-
         </div>
     );
 }
 
-export default TrasactionFail;
+export default TransactionFail;
