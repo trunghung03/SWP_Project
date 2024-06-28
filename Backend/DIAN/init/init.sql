5576287e58ad0bf33d0e6406f64e0c85a98fb6c4
Duyen9904 <mimitrucduyen@gmail.com>
2024-06-28 13:07:26 +0700

    add docker for deployment

--- Backend/DIAN/init/init.sql
+++ Backend/DIAN/init/init.sql
@@ -6,7 +6,7 @@ BEGIN
     -- If the database exists, drop it
     ALTER DATABASE DIAN SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
     DROP DATABASE DIAN;
-END
+ENDd
 GO
 -- Create the new database
 CREATE DATABASE DIAN;
